f6699796f0f2551147c37d739b00066e5f47b700
pokaahaha <men101@protonmail.com>
2023-01-27 20:56:01 +0300

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import Link from 'next/link';
+import React from "react";
+import { Typewriter, Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
 
-type Props = {}
+
+type Props = {};
 
 const Hero = (props: Props) => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, my Name's Dima",
+      "Guy-who-loves-Coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
-export default Hero
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://klike.net/uploads/posts/2021-12/1638345150_3.jpg"
+        alt=""
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-5xl font-semibold px-10">
+          <span className="me-3">{text}</span>
+          <Cursor cursorColor="#f7ab0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>{" "}
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href='projects'>
+            <button className="heroButton">Projects</button>{" "}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ import Hero from '@/components/Hero';
 export default function Home() {
   return (
     <>
-      <div>
+      <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
         <Head>
           <title>Portfolio App</title>
         </Head>
         {/* Header */}
         <Header />
         {/* Hero */}
-        <section id="hero">
+        <section id="hero" className='snap-center'>
           <Hero />
         </section>
         {/* About */}
